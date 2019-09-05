342e67ed81c0d78e7a9e46c5b14ecd2e591a984a
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-09-05 13:18:38 -0400

    Initial Commit

---
+++ test-sap-viewrouter-suiTests/test_sap_viewrouter_suiTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_sap_viewrouter_suiTests.swift
+//  test-sap-viewrouter-suiTests
+//
+//  Created by Tommie Carter on 9/5/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import XCTest
+@testable import test_sap_viewrouter_sui
+
+class test_sap_viewrouter_suiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
